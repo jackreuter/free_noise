7029670854429619b25d250a443f2f3ec77579bf
jreuter <jreuter@wesleyan.edu>
2014-03-01 12:55:01 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FreeNoise::Application.config.secret_key_base = '7b6a4c0475bb331ea1b23a86e9f574c9b14c72dc27dc3806deebfd0a4923bc62b1733221c55b28a2bb54d8799f66679a13c214fb7bde8db527755be004fc9474'
